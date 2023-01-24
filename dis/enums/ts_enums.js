0e8c3739b5566d92514d814ffdf82e34736dd63e
swarupyeole11 <swarupyeole11@gmail.com>
2023-01-25 00:06:22 +0530

    enums and tuples

---
+++ dis/enums/ts_enums.js
@@ -0,0 +1,9 @@
+"use strict";
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+console.log(Size.Medium);
+//# sourceMappingURL=ts_enums.js.map
